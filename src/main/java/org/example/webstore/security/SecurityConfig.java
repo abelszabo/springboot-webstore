a9ee89ef796a2efa97b090a553a72aad3ab3efef
Abel Szabo <abel.szabo.1982@gmail.com>
2026-03-13 13:01:36 +0100

    oidc

--- src/main/java/org/example/webstore/security/SecurityConfig.java
+++ src/main/java/org/example/webstore/security/SecurityConfig.java
@@ -2,9 +2,11 @@ package org.example.webstore.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -23,20 +25,28 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
             throws Exception {
 
         http
+                .securityMatcher("/api/**")
                 .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/actuator/**").hasRole("ADMIN")
                         .requestMatchers(
-                                "/actuator/**",
-                                "/health",
-                                "/info",
+//                                "/actuator/**",
+//                                "/health",
+//                                "/info",
                                 "/api/products/**",
-                                "/api/order/**",
-                                "/api/user/**"
+                                "/api/user/**", // FIXME
+                                "/api/order/**" // FIXME authenticated
+//                                "/api/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
